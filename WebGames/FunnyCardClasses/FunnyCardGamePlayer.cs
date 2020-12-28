6701bdc156cfe19bf25a8fa2a2baf692cf25cbb0
Dáire McDermott <60854290+qwertydaz@users.noreply.github.com>
2020-12-28 00:19:45 +0000

    Hand.cs and FunnyCardGamePlayer.cs
    Implementation of 2 classes from TemplateCode.txt

---
+++ WebGames/FunnyCardClasses/FunnyCardGamePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebGames.Basics;
+
+namespace WebGames.FunnyCardClasses
+{
+	class FunnyCardGamePlayer : Player
+	{
+		Hand playerHand;
+
+		public FunnyCardGamePlayer(string name, string ip)
+		{
+			string playerName = name;
+			string playerIP = ip;
+			Hand playerHand = new Hand();
+		}
+	}
+}
---
+++ WebGames/FunnyCardClasses/Hand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebGames.FunnyCardClasses
+{
+    class Hand
+    {
+		Card[] cards;
+		public Hand()
+		{
+			Card[] cards = new Card[10];
+		}
+
+		public Card[] getCards()
+		{
+			return cards;
+		}
+
+		private void refreshCards()
+		{
+		}
+	}
+}
